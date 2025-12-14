8f1a27f35f2db9bd09e2df3826224b691689b149
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-12-14 18:46:14 +0000

    routing

--- users/consumers.py
+++ users/consumers.py
@@ -6,15 +6,16 @@ from .models import NewsUsers, Message
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        # Sender id URL-dən alınır
+        # URL-dən sender id alınır
         self.sender_id = int(self.scope['url_route']['kwargs']['user_id'])
         self.sender = await database_sync_to_async(NewsUsers.objects.get)(id=self.sender_id)
         await self.accept()
+        print(f"User {self.sender.username} WebSocket-ə qoşuldu")
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         msg_type = data.get('type')
-        
+
         if msg_type == "chat_message":
             await self.handle_chat_message(data)
         elif msg_type == "load_messages":
@@ -24,6 +25,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message_text = data['message']
         receiver_name = data['receiver_name']
 
+        # Receiver user
         receiver = await database_sync_to_async(NewsUsers.objects.get)(username=receiver_name)
 
         # Mesajı DB-də saxla
@@ -33,18 +35,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
             text=message_text
         )
 
-        # Room adı yaradın
+        # Room adı (iki user üçün unikal)
         room_name = f"{min(self.sender.id, receiver.id)}_{max(self.sender.id, receiver.id)}"
         room_group_name = f"chat_{room_name}"
 
-        # Özünü qrupa əlavə et
+        # Qrupa əlavə et (yeni user join olduqda)
         await self.channel_layer.group_add(room_group_name, self.channel_name)
 
         # Mesajı qrupa göndər
         await self.channel_layer.group_send(
             room_group_name,
             {
-                'type': 'chat_message',
+                'type': 'chat_message_event',
                 'message': message_text,
                 'sender': self.sender.username
             }
@@ -56,10 +58,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Mesajları DB-dən al
         messages = await database_sync_to_async(
-            lambda: list(Message.objects.filter(
-                sender__in=[self.sender, receiver],
-                receiver__in=[self.sender, receiver]
-            ).order_by('id').values('sender__username', 'text'))
+            lambda: list(
+                Message.objects.filter(
+                    sender__in=[self.sender, receiver],
+                    receiver__in=[self.sender, receiver]
+                ).order_by('id').values('sender__username', 'text')
+            )
         )()
 
         formatted_messages = [{'sender': m['sender__username'], 'text': m['text']} for m in messages]
@@ -69,7 +73,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'messages': formatted_messages
         }))
 
-    async def chat_message(self, event):
+    async def chat_message_event(self, event):
+        # Qrupdan gələn mesajları frontend-ə göndər
         await self.send(text_data=json.dumps({
             'type': 'chat_message',
             'message': event['message'],
--- users/routing.py
+++ users/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    # User-in id-si URL parametri kimi ötürülür
     re_path(r'ws/chat/(?P<user_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
