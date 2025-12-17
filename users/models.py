896b4a9d3c660173ee4417954c7293c190c121fe
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-12-17 08:09:07 +0000

    models.py

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password, check_password
 class NewsUsers(models.Model):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=128, null=False)
-    email = models.CharField(max_length=254, unique=True)
+    email = models.CharField(max_length=254, unique=True, null=True, blank=False)
 
     class Meta:
         db_table = 'login'
