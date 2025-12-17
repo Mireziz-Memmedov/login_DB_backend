305c7bb8a0000557922b7102e7bf2226bd71c3cc
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-12-17 07:32:23 +0000

    asgi.py

--- log/asgi.py
+++ log/asgi.py
@@ -1,23 +1,7 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINzGS_MODULE", "log.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsapi.settings")
 
-import django
-django.setup()
-import users.routing
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            users.routing.websocket_urlpatterns
-        )
-    ),
-})
-
-
-
-# application = get_asgi_application()
+application = get_asgi_application()
