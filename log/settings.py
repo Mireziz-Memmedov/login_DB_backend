7e3b10c046ef747b84d363e1dded4e343fde0484
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-01-03 16:03:01 +0000

    setting.py

--- log/settings.py
+++ log/settings.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -142,7 +143,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587            
 EMAIL_USE_TLS = True   
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "astroping2025@gmail.com"
+EMAIL_HOST_PASSWORD = "drda nbuj dxyd pidw"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
