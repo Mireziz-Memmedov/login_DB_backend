c051a06343a2dfe1404a732474c5621e13081627
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-01-19 16:32:13 +0000

    models.py

--- users/models.py
+++ users/models.py
@@ -30,7 +30,7 @@ class Message(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
     deleted_for = models.JSONField(default=list)
-    # is_unsend = models.BooleanField(default=False)
+    is_unsend = models.BooleanField(default=False)
     
     class Meta:
         ordering = ['timestamp']
