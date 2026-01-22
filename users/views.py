61b9a72b4045a426aafe2bc4f7e6c9c54189f678
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-01-22 15:58:23 +0000

    view

--- users/views.py
+++ users/views.py
@@ -318,13 +318,13 @@ def unsend_chat(request):
 @api_view(['POST'])
 def delete_profile_chats(request):
     try:
-        current_user_id = int(request.data.get('user_id'))
+        current_user_id = request.data.get('user_id')
         target_username = request.data.get('target_username')
     except (TypeError, ValueError):
         return Response({'success': False, 'error': 'ID düzgün deyil'}) 
 
     try:
-        user = NewsUsers.objects.get(id=current_user_id)
+        user = NewsUsers.objects.get(id=int(current_user_id))
         target_user = NewsUsers.objects.get(username=target_username)
 
         messages = Message.objects.filter(
@@ -332,15 +332,18 @@ def delete_profile_chats(request):
             Q(sender=target_user, receiver=user)
         )
 
+        uid_str = str(current_user_id)
+
         for msg in messages:
-            if current_user_id not in msg.deleted_for:
-                msg.deleted_for.append(current_user_id)
+            if uid_str not in msg.deleted_for:
+                msg.deleted_for.append(uid_str)
                 msg.save(update_fields=['deleted_for'])
 
         return Response({'success': True})
 
     except NewsUsers.DoesNotExist:
         return Response({'success': False, 'error': 'İstifadəçi tapılmadı'})
+
 
 
     
