af25521d93bc1f9231a8b09584c225727580e2dc
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-02-23 15:46:04 +0000

    urls

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('recent-chats/', views.recent_chats, name='recent_chats'),
     path('send-message/', views.send_message, name='send_message'),
     path('get-messages/', views.get_messages, name='get_messages'),
+    path('update-activity/', views.update_activity, name='update_activity'),
     path('user_status/', views.user_status, name='user_status'),
     path('logout/', views.logout, name='logout'),
     path('forgot-check/', views.forgot_check, name='forgot_check'),
