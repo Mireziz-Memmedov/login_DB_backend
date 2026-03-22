b92ed1f34526c7f8d0d851088b9397704f2fcdcd
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-03-22 13:54:45 +0000

    setting.py

--- log/settings.py
+++ log/settings.py
@@ -23,6 +23,13 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://127.0.0.1:8000",
+    "https://localhost:8000",
+]
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
